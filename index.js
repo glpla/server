2d4c78527fc87fbd9c0c65b81cb3c66041b7123a
大树 <oncwnuI5Hn10fPVBg9FD25wICw88@git.weixin.qq.com>
2025-08-11 10:01:49 +0800

    basic service

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-const express = require('express');
-const fs = require('fs');
-const cors = require('cors');
+const express = require("express");
+const fs = require("fs");
 
 const app = express();
-const PORT = 3000
-let data = fs.readFileSync('./public/data/coffee.json');
-let jsonData = JSON.parse(data);
+const PORT = 3000;
+app.use(express.static("./public"));
 
-
-app.use(express.static('./public'));
-app.use(express.json());
-app.use(cors());
-
-app.get('/getCoffee', (req, res) => {
-  // res.header('Access-Control-Allow-Origin', '*')
-  const serverAddress = `${req.protocol}://${req.get('host')}`;
-  res.status(200).json({
-    "code": 0,
-    "msg": "success",
-    "data": jsonData,
-    "serverAddress": serverAddress
-  })
-})
 app.listen(PORT, () => {
   console.log(`server is now running on http://127.0.0.1:${PORT}`);
 });
